f6e8a3efb5416a03c4178358cf1d8630d4f30532
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-01-19 00:20:53 +0300

    initial templet of project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloWebChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,6 @@
+﻿namespace HelloWebChat
+{
+    public class Startup
+    {
+    }
+}
